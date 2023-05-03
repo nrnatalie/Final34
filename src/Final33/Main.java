f8f5c20ee61670f41da9c665d0b79d68ae48c7db
Nathalie Russo <johndoe@example.com>
2023-05-03 12:53:51 +0300

    Add result

--- src/Final33/Main.java
+++ src/Final33/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
   public static void main(String[] args) {
     Soda lemonad = new Soda("malina");
+    System.out.println(lemonad);
+    System.out.println(lemonad.getFood());
+    lemonad.setFood("clubnica");
+    System.out.println(lemonad.getFood());
   }
 
 }
+//Final33.Soda@5f184fc6
+//malina
+//clubnica
