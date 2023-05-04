1ac3ddd12941583872b1fdb986f0cfc3eeda3328
Nathalie Russo <johndoe@example.com>
2023-05-04 16:35:39 +0300

    Add metod boolean checkSides

--- src/Final34/Triangle.java
+++ src/Final34/Triangle.java
@@ -21,14 +21,45 @@ public class Triangle {
 
   private final int c;
 
-  public Triangle(int a, int b, int c, int a1, int b1, int c1) throws IllegalArgumentException {
-    this.a = a; // 2-задали конструктор
-    this.b = b;
-    this.c = c;
+  public Triangle(int a, int b, int c) throws IllegalArgumentException {
 
     if ((a < 0) || (b < 0) || (c < 0)) {
       throw new IllegalArgumentException("Длина не может быть отрицательной");
 
     }
+
+    if (a == 0 || (b == 0) || (c == 0)) {
+      throw new IllegalArgumentException("Длина не может быть равна нулю");
+    }
+
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    checkSides(a, b, c);
+  }
+
+
+  public static boolean checkSides(int a, int b, int c) {
+
+    if ((a < c + b) || (c < a + b) || (b < a + b)) {
+      throw new IllegalArgumentException("Ошибка " + a + b + c);
+    }
+    return checkSides(a, b, c);
+  }
+
+  public int getA() {
+    return a;
+  }
+
+  public int getB() {
+    return b;
+  }
+
+  public int getC() {
+    return c;
   }
 }
+
+
+
+
