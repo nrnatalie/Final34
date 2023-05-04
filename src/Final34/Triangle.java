eff571cfe1238a0d49223246d55f7d5479198896
Nathalie Russo <johndoe@example.com>
2023-05-04 16:28:44 +0300

    Add atribut and constructor

--- src/Final34/Triangle.java
+++ src/Final34/Triangle.java
@@ -16,4 +16,19 @@ package Final34;
 // или выкинуть исключение.
 public class Triangle {
 
+  private final int a;
+  private final int b;   // atribut
+
+  private final int c;
+
+  public Triangle(int a, int b, int c, int a1, int b1, int c1) throws IllegalArgumentException {
+    this.a = a; // 2-задали конструктор
+    this.b = b;
+    this.c = c;
+
+    if ((a < 0) || (b < 0) || (c < 0)) {
+      throw new IllegalArgumentException("Длина не может быть отрицательной");
+
+    }
+  }
 }
