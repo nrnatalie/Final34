55145a5a65af9ba35a594c12d225530e9afd46ef
Nathalie Russo <johndoe@example.com>
2023-05-04 16:37:37 +0300

    Add main

--- src/Final34/Main.java
+++ src/Final34/Main.java
@@ -2,4 +2,17 @@ package Final34;
 
 public class Main {
 
+  public static void main(String[] args) {
+    Triangle1 triangle = new Triangle1(3, 3, 8);
+    System.out.println(triangle);
+    System.out.println(triangle.getA() + " :" + triangle.getB());
+
+    System.out.println(triangle.getB() + ": " + triangle.getA());
+    System.out.println(triangle.getC() + ":" + triangle.getC());
+
+  }
+
 }
+
+
+
