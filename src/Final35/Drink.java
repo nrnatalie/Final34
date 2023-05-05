cfe219953e58007cb66316e29593dd239ac05f48
Nathalie Russo <johndoe@example.com>
2023-05-05 16:10:37 +0300

    Add Drink "пшш"

---
+++ src/Final35/Drink.java
@@ -0,0 +1,16 @@
+package Final35;
+
+
+public class Drink extends Food {
+
+  public Drink(String name) {
+    super(name);
+  }
+
+  public void soundSoda() {
+    System.out.println(getName() + "пшш");
+  }
+
+}
+
+
--- src/Final35/Main.java
+++ src/Final35/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
   public static void main(String[] args) {
     Food food = new Food("Pizza");
+    food.eatFood();
+    Drink drink = new Drink(" Soda: ");
+    drink.soundSoda();
 
   }
 }
