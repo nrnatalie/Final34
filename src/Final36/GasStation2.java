498cbd54f4efa99c67fa7a70ef2db59dd14056ad
Nathalie Russo <johndoe@example.com>
2023-05-06 18:43:28 +0300

    feat: add orders number

--- src/Final36/GasStation2.java
+++ src/Final36/GasStation2.java
@@ -15,36 +15,56 @@ public class GasStation2 {
     System.out.println("Добро пожаловать! Как вас зовут?");
     // String name = br.readLine();
     String name = scanner.nextLine();
-    System.out.println("Сколько литров топлива вам нужно?");
-    double quantity = 0.0;
-    //double quantity = Double.parseDouble(br.readLine());
-    while (!scanner.hasNextDouble()) {// пока всё плохо,
+    System.out.print("Введите количество заказов: ");
+    while (!scanner.hasNextInt()) {
       String wrongLine = scanner.nextLine();
-
-      // String wrongLine = br.readLine(); // пропустить строку
-      System.out.println("Неправильный формат дробного числа: " + wrongLine);
-      System.out.println("Введите количество топлива (в литрах):");
+      System.out.println("Неправильный формат целого числа:" + wrongLine);
+      System.out.print("Введите количество заказов: ");
     }
-    quantity = scanner.nextDouble();
-    scanner.nextLine();
-    System.out.println("Как будете оплачивать, картой или наличными?");
-    String paymentMethod = scanner.nextLine();
-    //String paymentMethod = br.readLine();
-    boolean byCard = (paymentMethod.toLowerCase().contains("карт"));
+    int orders = scanner.nextInt();
+    //int counter = 0; //у меня есть счетчик пока значение ноль  //1.вариант с while
+    //while (counter < orders) {
+    // for (командаПередЦиклом; условиеПовторения; командаПослеШага)
+    for (int counter = 0; counter < orders; ++counter) {        //2. вариант c for
+      // Order order = Order.readOrder(name, scanner);
+      //order.print();
 
-    System.out.println("=== Ваш заказ ===");
-    System.out.println("Заказчик: " + name);
-    System.out.println("Количество топлива: " + quantity + " л");
-    double total = quantity * 2;
-    System.out.println("Стоимость заказа: " + total + " EUR");
-    if (byCard) {
-      System.out.println("Заказ должен быть оплачен картой");
-    } else {
-      System.out.println("Заказ должен быть оплачен наличными");
+      System.out.println("Сколько литров топлива вам нужно?");
+      //double quantity = 0.0;
+      //double quantity = Double.parseDouble(br.readLine());
+      while (!scanner.hasNextDouble()) {// пока всё плохо,
+        String wrongLine = scanner.nextLine();
+
+        // String wrongLine = br.readLine(); // пропустить строку
+        System.out.println("Неправильный формат дробного числа: " + wrongLine);
+        System.out.println("Введите количество топлива (в литрах):");
+      }
+      double quantity = scanner.nextDouble();
+      scanner.nextLine();
+      System.out.println("Как будете оплачивать, картой или наличными?");
+      String paymentMethod = scanner.nextLine();
+      //String paymentMethod = br.readLine();
+      boolean byCard = (paymentMethod.toLowerCase().contains("карт"));// byCard называется флажок
+
+      System.out.println("=== Ваш заказ ===");
+      System.out.println("Заказчик: " + name);
+      System.out.println("Количество топлива: " + quantity + " л");
+      double total = quantity * 2;
+      System.out.println("Стоимость заказа: " + total + " EUR");
+      if (byCard) {   // и этот флажок мы всавляем сюда
+        System.out.println("Заказ должен быть оплачен картой");
+      } else {
+        System.out.println("Заказ должен быть оплачен наличными");
+      }
+      //++counter; // это относилось к циклу while
+
     }
+
 
   }
-
 }
+
+
+
 
 
--- src/Final36/Order2.java
+++ src/Final36/Order2.java
@@ -1,6 +1,25 @@
-package Final36;
+/*package Final36;
+
+import Final36.Order.PaymentMethod;
+import java.io.BufferedReader;
+import java.nio.Buffer;
+import java.util.Scanner;
 
 public class Order2 {
 
+  private String name;
+  private double quantity;  // это количество
+  private PaymentMethod paymentMethod;
 
+  public Order(String name, double quantity, PaymentMethod paymentMethod) {
+    this.name = name;
+    this.quantity = quantity;     //1.
+    this.paymentMethod = paymentMethod;
+
+    public static Order readOrder (String name, Scanner scanner){
+      double quantity = readFuelQuantity(scanner);
+    }
+  }
 }
+*/
+
