7a8e0c6c2d2a38f168620a8f9b31e8927c47e26c
Nathalie Russo <johndoe@example.com>
2023-05-09 16:41:22 +0300

    Add class Human and getter and setter

---
+++ src/Final37/Human.java
@@ -0,0 +1,24 @@
+package Final37;
+
+
+public class Human {
+
+  private String name;
+
+  public Human(String name) {
+    this.name = name;
+
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName() {
+    this.name = name;
+  }
+
+
+}
+
+
