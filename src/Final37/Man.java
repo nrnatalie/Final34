87af1a6042bd4b8d8f6edb9553459712ade4a62a
Nathalie Russo <johndoe@example.com>
2023-05-09 16:43:57 +0300

    Add class Man and Woman

---
+++ src/Final37/Man.java
@@ -0,0 +1,14 @@
+package Final37;
+
+import Homework.Human;
+
+
+public class Man extends Human {
+
+  public Man(String name) {
+    super(name);
+
+
+  }
+
+}
---
+++ src/Final37/Woman.java
@@ -0,0 +1,10 @@
+package Final37;
+
+
+public class Woman extends Human {
+
+  public Woman(String name) {
+    super(name);
+  }
+
+}
