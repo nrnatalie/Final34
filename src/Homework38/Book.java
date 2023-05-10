1e38e0fc2d6e45a2c506b0814e72e6924c807d4a
Nathalie Russo <johndoe@example.com>
2023-05-10 16:22:40 +0300

    Add  toString and boolean equals

--- src/Homework38/Book.java
+++ src/Homework38/Book.java
@@ -1,5 +1,7 @@
 package Homework38;
 
+import Domaska.Book1;
+
 public class Book {
 
   private String name;
@@ -23,4 +25,21 @@ public class Book {
     this.author = author;
   }
 
+  @Override
+  public String toString() {
+    return " Книга: " + name + "," + " Автор: " + author;
+  }
+
+  @Override
+  public boolean equals(Object obj) { // метод equals переопределяет сравнение обьектов
+    if (this == obj) { // сравнивает обьект сам с собой
+      return true;
+    }
+    if (!(obj instanceof Book1 book1)) {
+      // если вдруг// наш обьект не является book1
+      return false; // то сразу возвращаем false
+    }
+    return name.equals(book1.getName()) && author.equals(book1.getAuthor());// у
+
+  }
 }
