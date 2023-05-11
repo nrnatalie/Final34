5a12c594cc12a2e4ef281639d1053be69074fafa
Nathalie Russo <johndoe@example.com>
2023-05-11 17:08:36 +0300

    Add Main

---
+++ src/Domaska39/Main.java
@@ -0,0 +1,35 @@
+package Domaska39;
+
+import Final39.Homework38.Domaska.ApartamentHouse1;
+import Final39.Homework38.Domaska.House1;
+import Final39.Homework38.Domaska.PrivateHouse1;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+
+  public static void main(String[] args) {
+    House1 privateHouse = new PrivateHouse1("Студенческая");
+    System.out.println(privateHouse);
+    System.out.println();
+    House1 apartamentHouse = new ApartamentHouse1("Комсомольская ", 34, 3);
+    System.out.printf("Количество подъездов: %d", 3);
+    System.out.println();
+
+    List<House1> name = new ArrayList<>();
+    name.add(privateHouse);
+    name.add(apartamentHouse);
+    System.out.println(name);
+
+    for (House1 elite : name) {
+      elite.House(); // перезапись
+
+
+    }
+
+
+  }
+
+}
+
