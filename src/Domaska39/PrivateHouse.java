ee1659d4692698ac6c6346c6320c8c51567765e4
Nathalie Russo <johndoe@example.com>
2023-05-11 17:01:33 +0300

    Add PrivateHouse toString

---
+++ src/Domaska39/PrivateHouse.java
@@ -0,0 +1,19 @@
+package Domaska39;
+
+import Final39.Homework38.Domaska.House1;
+
+public class PrivateHouse {
+
+  public class PrivateHouse1 extends House1 {
+
+    public PrivateHouse1(String address) { // частный дом
+      super(address);
+    }
+
+    @Override
+    public String toString() {
+      return "Вы снесли частный дом: " + getAddress();
+    }
+
+  }
+}
