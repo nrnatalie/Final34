8fa4c7856aa6f417b562e7c3e5e41a4153c6bd97
Nathalie Russo <johndoe@example.com>
2023-05-12 13:18:01 +0300

    Add imlements CanRun

--- src/Superman/CanRun.java
+++ src/Superman/CanRun.java
@@ -1,5 +1,22 @@
 package Superman;
 
-public class CanRun {
+public class CanRun implements Superman {
+
+  public String heigt;
+
+  public CanRun(String heigt) {
+    this.heigt = heigt;
+  }
+
+  public void Run() {
+    System.out.println("Пингвин:" + "Умеет бегать");
+
+  }
+
+  @Override
+  public void showSuperman() {
+    System.out.println("heigt" + this.heigt);
+
+  }
 
 }
