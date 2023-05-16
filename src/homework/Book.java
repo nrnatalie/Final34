9f27b88d35bad2495197f1857968c39d8c3bf2d9
Nathalie Russo <johndoe@example.com>
2023-05-16 17:07:37 +0300

    Add Finich

--- src/homework/Book.java
+++ src/homework/Book.java
@@ -7,64 +7,62 @@ package homework;
 //
 //Реализуйте интерфейс для сравнения книг: они должны сортироваться по авторам,
 // а если авторы совпадают - по названиям (и там, и там - по алфавиту, "в словарном порядке").
-  public class Book implements Comparable<Book> {
+public class Book implements Comparable<Book> {
 
-    private final String name;
-    private final String author;
-    private final int amount;
+  private final String name;
+  private final String author;
+  private final int amount;
 
-    public Book(String name, String author, int amount) {
-      this.name = name;
-      this.author = author;
-      this.amount = amount;
+  public Book(String name, String author, int amount) {
+    this.name = name;
+    this.author = author;
+    this.amount = amount;
 
-    }
+  }
 
-    public String getName() {
-      return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String getAuthor() {
-      return author;
-    }
+  public String getAuthor() {
+    return author;
+  }
 
-    public int getAmount() {
-      return amount;
-    }
+  public int getAmount() {
+    return amount;
+  }
 
-    @Override
-    public String toString() {
-      return "Book{" +
-          "name='" + name + '\'' +
-          ", author='" + author + '\'' +
-          ", amount=" + amount +
-          '}';
-
-
-    }
-
-    @Override
-    public int compareTo(Book oder) {
-      //return this.amount - oder.amount;// для сравнения количества
-      return author.compareTo(oder.author); // для сранения авторов
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-      if (!(obj instanceof Book oder)) {
-        return false;
-      }
-      //return this.amount == oder.amount;// количество
-      return author.equals(oder.author);
-    }
-
-    @Override
-    public int hashCode() {
-      //return amount; //количество
-      return author.hashCode();
-    }
+  @Override
+  public String toString() {
+    return "Book{" +
+        "name='" + name + '\'' +
+        ", author='" + author + '\'' +
+        ", amount=" + amount +
+        '}';
 
 
   }
+
+  @Override
+  public int compareTo(Book oder) {
+    //return this.amount - oder.amount;// для сравнения количества
+    return author.compareTo(oder.author); // для сранения авторов
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Book oder)) {
+      return false;
+    }
+    //return this.amount == oder.amount;// количество
+    return author.equals(oder.author);
+  }
+
+  @Override
+  public int hashCode() {
+    //return amount; //количество
+    return author.hashCode();
+  }
+}
 
 
