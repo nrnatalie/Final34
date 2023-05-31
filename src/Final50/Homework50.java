6538666607e8da78e8e6723dc34da5fd2194dec7
Nathalie Russo <johndoe@example.com>
2023-05-31 16:55:38 +0300

    Add task3

--- src/Final50/Homework50.java
+++ src/Final50/Homework50.java
@@ -1,7 +1,9 @@
 package Final50;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Homework50 {
@@ -46,5 +48,43 @@ public class Homework50 {
 //[2, 4, 6, 8, 10]
 //-------------------------------
 
+    //Задача 3
+//Прочитать с клавиатуры сначала количество записей в телефонной книге,
+// а затем сами записи (номер телефона и имя).
+//
+//Сохранить эти записи в словаре (телефон (строка) - имя (строка)).
+//
+//Спросить имя для удаления.
+//
+//Удалить из словаря все телефоны, имя для которых совпадает с указанным.
+//
+//Вывести получившийся словарь.
+
+    Map<String, String> persons = new HashMap<>();
+
+    persons.put("11111", " Даша");
+    persons.put("22222", " Лена");
+    persons.put("33333", " Оля");
+    persons.put("44444", " Рита");
+    persons.put("55555", " Света");
+    System.out.println(persons);
+    System.out.println(persons.get("22222" + "Вы хотели бы удалить?"));
+    System.out.println(persons.remove("22222"));
+    System.out.println(persons.values());
+//5
+//1
+//2
+//3
+//4
+//5
+//{44444= Рита, 33333= Оля, 55555= Света, 11111= Даша, 22222= Лена}
+// Лена
+// Лена
+//[ Рита,  Оля,  Света,  Даша]
+
   }
+
+
 }
+
+
